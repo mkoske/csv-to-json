5bfe716f03183d0fc3398fc1f13ebb8f3cef972d
Miika Koskela <miika.s.koskela@gmail.com>
2020-01-05 14:50:48 +0200

    Working on conversion script

--- app.js
+++ app.js
@@ -5,11 +5,49 @@
  * @copyright 2020 Tampereen kaupunginkirjasto
  * @license MIT
  */
+
+function getCurrentDateString() {
+
+  const date = new Date();
+
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let dayno = date.getDate();
+
+  if (month.toString().length == 1) {
+    month = "0" + month.toString();
+  }
+
+  if (dayno.toString().length == 1) {
+    dayno = "0" + dayno.toString();
+  }
+
+  return "" + year + "-" + month + "-" + dayno;
+}
+
 (function () {
 
-  // TODO: Map to column names which will be used on JSON
-  const columnMap = {
-    "author": 0
+  const result = {
+    "books": {
+      "title": "Perhelukudiplomin kirjalista",
+      "description": "",
+      "updated": getCurrentDateString(),
+      "books": [],
+    }
+  };
+
+  const bookTemplate = {
+    "title": "",
+    "author": "",
+    "updated": "",
+    "coverImageUrl": "",
+    "ageGroup": "",
+    "themes": [],
+    "description": "",
+    "availabilityUrl": "",
+    "alternatives": [],
+    "additionalInformation": "",
+    "helpText": ""
   };
 
   const files = document.querySelector("input[type='file']");
@@ -27,7 +65,58 @@
       for (let i = 0; i < lines.length; i++) {
         const element = lines[i];
         const columns = element.split('\t');
+
+        // This copies or clones bookTemplate object
+        let book = JSON.parse(JSON.stringify(bookTemplate));
+
+        book.title = columns[0];
+        book.author = columns[1];
+        book.updated = columns[2]
+        book.coverImageUrl = columns[3];
+
+        book.ageGroup = columns[4].split(",").map(function (item) {
+          return item.trim();
+        });
+
+        book.themes = columns[5].split(",").map(function (item) {
+          return item.trim();
+        });
+
+        book.description = columns[6];
+        book.availabilityUrl = columns[7];
+
+        book.additionalInformation = columns[8];
+        book.helpText = columns[9];
+
+        // TODO: This could be just looped over from 10 to 12, inclusive
+        let alternative = [];
+
+        // TODO: If there's no alternative, the array is full of random stuff
+        // so add only, if there really is an alternative
+        alternative = columns[10].split(";");
+        book.alternatives.push({
+          "alternativeLabel": alternative[0],
+          "alternativeAvailabilityUrl": alternative[1]
+        })
+
+        alternative = columns[11].split(";");
+        book.alternatives.push({
+          "alternativeLabel": alternative[0],
+          "alternativeAvailabilityUrl": alternative[1]
+        })
+
+        alternative = columns[12].split(";");
+        book.alternatives.push({
+          "alternativeLabel": alternative[0],
+          "alternativeAvailabilityUrl": alternative[1]
+        })
+
+        result.books.books.push(book);
       }
+
+      console.log(result.books.books);
+      // TODO: Force download
+      const blob = new Blob([JSON.stringify(result)], { type: "text/json" });
 
     };
 
